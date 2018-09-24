a6d5fc78f79a61d250df18ebe4785c773929205b
Ajinkya-Sonawane <sonawaneajinks@gmail.com>
2018-09-24 23:59:40 +0530

    Initial App commit

---
+++ Group 04/APP/FakeNewsStance/app/src/main/java/com/fakenewstance/www/fakenewsstance/Feed.java
@@ -0,0 +1,13 @@
+package com.fakenewstance.www.fakenewsstance;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Feed extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed);
+    }
+}
